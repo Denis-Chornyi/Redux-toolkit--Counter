1b2183c5eae887256e26f707d16f985b5e0468ae
Denis Chornyi <chornyi.den@gmail.com>
2024-08-10 15:35:29 +0300

    fix

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Counter from '../counter/Counter';
-import './app.scss'
-
+import './app.scss';
 
 const App = () => {
   return (
-    <div className='page'>
-      <h2 className='page__title'>Redux Complete Tutorial</h2>
+    <div className="page">
+      <h2 className="page__title">Redux Complete Tutorial</h2>
       <Counter />
     </div>
   );
